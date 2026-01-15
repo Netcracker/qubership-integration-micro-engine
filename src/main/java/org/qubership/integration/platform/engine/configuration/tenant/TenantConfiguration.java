686ca1246fd9db756550315feffddd8d72ed25d6
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2026-01-15 11:40:13 +0300

    feat: added context propagation

--- src/main/java/org/qubership/integration/platform/engine/configuration/tenant/TenantConfiguration.java
+++ src/main/java/org/qubership/integration/platform/engine/configuration/tenant/TenantConfiguration.java
@@ -1,7 +1,7 @@
 package org.qubership.integration.platform.engine.configuration.tenant;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -9,18 +9,6 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 public class TenantConfiguration {
     @Getter
     @ConfigProperty(name = "tenant.default.id")
+    @NotBlank
     String defaultTenant;
-
-    @PostConstruct
-    public void init() {
-        // TODO configure tenant provider
-        //        if (StringUtils.isEmpty(defaultTenant)) {
-        //            throw new BeanInitializationException(
-        //                    "Default tenant is empty! Please specify application property [tenant.default.id]");
-        //        }
-        //
-        //        log.info("Default tenant id [tenant.default.id]: {}", defaultTenant);
-        //        this.defaultTenant = defaultTenant;
-        //        ContextManager.register(Collections.singletonList(new AppDefaultTenantProvider(defaultTenant)));
-    }
 }
