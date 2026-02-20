cf702201ed98c81b3313367d710a142c88a423b0
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2026-02-20 16:05:53 +0300

    fix: made consul token optional to aling with cloud core localdev

--- src/main/java/org/qubership/integration/platform/engine/consul/ConsulTokenStorage.java
+++ src/main/java/org/qubership/integration/platform/engine/consul/ConsulTokenStorage.java
@@ -5,15 +5,17 @@ import io.quarkus.arc.properties.IfBuildProperty;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.util.Optional;
+
 @ApplicationScoped
 @IfBuildProperty(name = "quarkus.consul-source-config.m2m.enabled", stringValue = "false")
 public class ConsulTokenStorage implements TokenStorage {
     @ConfigProperty(name = "consul.token")
-    String token;
+    Optional<String> token;
 
     @Override
     public String get() {
-        return token;
+        return token.orElse("");
     }
 
     @Override
