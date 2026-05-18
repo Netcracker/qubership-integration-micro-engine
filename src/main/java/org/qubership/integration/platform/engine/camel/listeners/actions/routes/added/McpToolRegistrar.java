91fcdf680c99c14af41096ff84f53c4f5ad35a86
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2026-05-18 16:46:22 +0300

    fix: updated MCP tool management for camelk

---
+++ src/main/java/org/qubership/integration/platform/engine/camel/listeners/actions/routes/added/McpToolRegistrar.java
@@ -0,0 +1,90 @@
+package org.qubership.integration.platform.engine.camel.listeners.actions.routes.added;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkiverse.mcp.server.MetaKey;
+import io.quarkiverse.mcp.server.ToolManager;
+import io.quarkiverse.mcp.server.ToolResponse;
+import io.smallrye.common.annotation.Identifier;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.spi.CamelEvent;
+import org.apache.commons.lang3.StringUtils;
+import org.qubership.integration.platform.engine.camel.listeners.EventProcessingAction;
+import org.qubership.integration.platform.engine.camel.listeners.qualifiers.OnRouteAdded;
+import org.qubership.integration.platform.engine.metadata.DeploymentInfo;
+import org.qubership.integration.platform.engine.metadata.ElementInfo;
+import org.qubership.integration.platform.engine.metadata.McpTriggerInfo;
+import org.qubership.integration.platform.engine.metadata.util.MetadataUtil;
+import org.qubership.integration.platform.engine.model.ChainElementType;
+
+import java.util.Map;
+
+@Slf4j
+@OnRouteAdded
+@ApplicationScoped
+public class McpToolRegistrar implements EventProcessingAction<CamelEvent.RouteAddedEvent> {
+    public static final String DEPLOYMENT_ID = "deploymentId";
+
+    @Inject
+    ToolManager toolManager;
+
+    @Inject
+    @Identifier("jsonMapper")
+    ObjectMapper objectMapper;
+
+    @Override
+    public void process(CamelEvent.RouteAddedEvent event) throws Exception {
+        DeploymentInfo deploymentInfo = MetadataUtil.getBean(event.getRoute(), DeploymentInfo.class);
+        MetadataUtil.getElementsInfo(event.getRoute())
+                .filter(McpToolRegistrar::isMcpTrigger)
+                .forEach(elementInfo -> {
+                    McpTriggerInfo mcpTriggerInfo = MetadataUtil.getBeanForElement(
+                            event.getRoute(), elementInfo.getId(), McpTriggerInfo.class);
+                    registerMcpTool(event.getRoute().getCamelContext(), deploymentInfo, elementInfo, mcpTriggerInfo);
+                });
+    }
+
+    private static boolean isMcpTrigger(ElementInfo elementInfo) {
+        ChainElementType elementType = ChainElementType.fromString(elementInfo.getType());
+        return ChainElementType.MCP_TRIGGER.equals(elementType);
+    }
+
+    public void registerMcpTool(CamelContext context, DeploymentInfo deploymentInfo, ElementInfo elementInfo, McpTriggerInfo mcpTriggerInfo) {
+        try {
+            ToolManager.ToolDefinition toolDefinition = toolManager.newTool(mcpTriggerInfo.getName());
+            toolDefinition
+                    .setDescription(mcpTriggerInfo.getDescription())
+                    .setTitle(mcpTriggerInfo.getTitle())
+                    .setAnnotations(new ToolManager.ToolAnnotations(
+                            mcpTriggerInfo.getTitle(),
+                            mcpTriggerInfo.getReadOnly(),
+                            mcpTriggerInfo.getDestructive(),
+                            mcpTriggerInfo.getIdempotent(),
+                            mcpTriggerInfo.getOpenWorld()))
+                    .setMetadata(Map.of(MetaKey.of(DEPLOYMENT_ID), deploymentInfo.getId()))
+                    .setInputSchema(objectMapper.readTree(mcpTriggerInfo.getInputSchema()));
+            String outputSchema = mcpTriggerInfo.getOutputSchema();
+            boolean hasOutputSchema = StringUtils.isNotBlank(outputSchema);
+            if (hasOutputSchema) {
+                toolDefinition.setOutputSchema(objectMapper.readTree(outputSchema));
+            }
+            toolDefinition.setHandler((arguments) -> {
+                ProducerTemplate producerTemplate = context.createProducerTemplate();
+                String endpointUri = "direct:" + elementInfo.getId();
+                Exchange result = producerTemplate.request(endpointUri, exchange ->
+                        exchange.getIn().setBody(arguments.args()));
+                return hasOutputSchema
+                        ? ToolResponse.structuredSuccess(result.getMessage().getBody())
+                        : ToolResponse.success(result.getMessage().getBody(String.class));
+            });
+            toolDefinition.register();
+        } catch (JsonProcessingException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
